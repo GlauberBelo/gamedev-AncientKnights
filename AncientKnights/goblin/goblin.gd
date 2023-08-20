5836768086a59c46e0a36fdcd700147809c1fab7
GlauberBelo <wglauber.3d@gmail.com>
2023-08-20 16:04:10 -0300

    Spawning area attack

--- AncientKnights/goblin/goblin.gd
+++ AncientKnights/goblin/goblin.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+const ATTACK_AREA: PackedScene = preload("res://goblin/enemy_attack_area.tscn")
+const OFFSET: Vector2 = Vector2(0.5, 31)
+
+@onready var texture: Sprite2D = get_node("Texture")
 @onready var animation: AnimationPlayer = get_node("Animation")
 @onready var aux_animation_player: AnimationPlayer = get_node("AuxAnimationPlayer")
 
@@ -14,7 +18,7 @@ func _physics_process(_delta) -> void:
 	if can_die:
 		return
 		
-	if player_ref == null:
+	if player_ref == null or player_ref.can_die:
 		velocity = Vector2.ZERO
 		animate()
 		return
@@ -30,7 +34,18 @@ func _physics_process(_delta) -> void:
 	move_and_slide()
 	animate()
 
+func spawn_attack_area() -> void:
+	var attack_area = ATTACK_AREA.instantiate()
+	attack_area.position = OFFSET
+	add_child(attack_area)
+
 func animate() -> void:
+	if velocity.x > 0:
+		texture.flip_h = false
+	
+	if velocity.x < 0:
+		texture.flip_h = true
+	
 	if velocity != Vector2.ZERO:
 		animation.play("run")
 		return
