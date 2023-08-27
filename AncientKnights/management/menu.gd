38475861f24300cd7925115f7b622eff6001b7c7
GlauberBelo <wglauber.3d@gmail.com>
2023-08-26 23:14:09 -0300

    Implementing transition screen

--- AncientKnights/management/menu.gd
+++ AncientKnights/management/menu.gd
@@ -7,6 +7,8 @@ func _ready() -> void:
 func on_button_pressed(button_name: String) -> void:
 	match button_name:
 		"NewGame":
-			get_tree().change_scene_to_file("res://management/level.tscn")
+			transition_screen.scene_path = "res://management/level.tscn"
+			transition_screen.fade_in()
 		"Quit":
-			get_tree().quit()
+			transition_screen.can_quit = true
+			transition_screen.fade_in()
